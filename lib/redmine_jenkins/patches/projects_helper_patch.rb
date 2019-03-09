0378763a492b46330d7b1497dedd0f42912ec539
ayapapa <ayapapajapan@yahoo.co.jp>
2019-03-10 00:06:38 +0900

    update for rm4

--- lib/redmine_jenkins/patches/projects_helper_patch.rb
+++ lib/redmine_jenkins/patches/projects_helper_patch.rb
@@ -1,8 +1,8 @@
 require_dependency 'projects_helper'
 
-module RedmineJenkins
-  module Patches
-    module ProjectsHelperPatch
+module RedmineJenkinsProjectsHelperPatch
+  #module Patches
+    #module ProjectsHelperPatch
 
       #def self.included(base)
       #  base.send(:include, InstanceMethods)
@@ -16,28 +16,32 @@ module RedmineJenkins
       #end
 
 
-      module InstanceMethods
+      #module InstanceMethods
 
         #def project_settings_tabs_with_redmine_jenkins(&block)
         def project_settings_tabs
           #tabs = project_settings_tabs_without_redmine_jenkins(&block)
           tabs = super
-          tabs.push({
-            :name    => 'jenkins',
-            :action  => :edit_jenkins_settings,
-            :partial => 'projects/settings/redmine_jenkins',
-            :label   => :label_jenkins
-          })
-          tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
+          if @project.module_enabled?('jenkins')
+	        tabs.push({
+	          :name    => 'jenkins',
+	          :action  => :edit_jenkins_settings,
+	          :partial => 'projects/settings/redmine_jenkins',
+	          :label   => :label_jenkins
+	        }
+	        tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
+	      end
           tabs
         end
 
-      end
+      #end
 
-    end
-  end
+    #end
+  #end
 end
 
-unless ProjectsHelper.included_modules.include?(RedmineJenkins::Patches::ProjectsHelperPatch)
-  ProjectsHelper.send(:include, RedmineJenkins::Patches::ProjectsHelperPatch)
-end
+#unless ProjectsHelper.included_modules.include?(RedmineJenkins::Patches::ProjectsHelperPatch:InstanceMethods)
+#  ProjectsHelper.send(:include, RedmineJenkins::Patches::ProjectsHelperPatch:InstanceMethods)
+#end
+ProjectsHelper.prepend(RedmineJenkinsProjectsHelperPatch)
+~
