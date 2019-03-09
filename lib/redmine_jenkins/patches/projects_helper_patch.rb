e4892f3687be486952ce4c8869c35d650c1e1fd9
ayapapa <ayapapajapan@yahoo.co.jp>
2019-03-09 22:42:56 +0900

    update for rm4

--- lib/redmine_jenkins/patches/projects_helper_patch.rb
+++ lib/redmine_jenkins/patches/projects_helper_patch.rb
@@ -4,22 +4,24 @@ module RedmineJenkins
   module Patches
     module ProjectsHelperPatch
 
-      def self.included(base)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          unloadable
+      #def self.included(base)
+      #  base.send(:include, InstanceMethods)
+      #  base.class_eval do
+      #    unloadable
 
-          #alias_method_chain :project_settings_tabs, :redmine_jenkins
-          alias_method :project_settings_tabs_without_redmine_jenkins, :project_settings_tabs
-          alias_method :project_settings_tabs, :project_settings_tabs_with_redmine_jenkins
-        end
-      end
+      #    #alias_method_chain :project_settings_tabs, :redmine_jenkins
+      #    alias_method :project_settings_tabs_without_redmine_jenkins, :project_settings_tabs
+      #    alias_method :project_settings_tabs, :project_settings_tabs_with_redmine_jenkins
+      #  end
+      #end
 
 
       module InstanceMethods
 
-        def project_settings_tabs_with_redmine_jenkins(&block)
-          tabs = project_settings_tabs_without_redmine_jenkins(&block)
+        #def project_settings_tabs_with_redmine_jenkins(&block)
+        def project_settings_tabs
+          #tabs = project_settings_tabs_without_redmine_jenkins(&block)
+          tabs = super
           tabs.push({
             :name    => 'jenkins',
             :action  => :edit_jenkins_settings,
